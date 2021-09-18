b6f8858076593de4aee495901f71f17b72c9f3b9
19cashj <67474732+19cashj@users.noreply.github.com>
2021-09-18 13:28:59 -0400

    Added more DOM methods

--- dom-methods/dom-methods.js
+++ dom-methods/dom-methods.js
@@ -13,16 +13,29 @@ document.getElementsByTagName("p"); //Find all elements by HTML tag name
 
 element.innerHTML =  	//Change the inner HTML of an element
 element.textContent = //Change the text content of an HTML element
-element.attribute =  	//Change a current existing attribute value of an element
 element.style.property = "" 	//Change the style of an element
 element.value //Gets the current value of an element like text
 
+element.attribute =  	//Change a current existing attribute value of an element
+element.getAttribute('id')  //Alternative to element.attribute
 element.setAttribute(attribute, value) 	//Add and set a new attribute of an element
+element.remoteAttribute(attribute) //Removes an attribute
 
-document.createElement("p") 	//Create an HTML element
-document.removeChild(element) 	//Remove an HTML element
+document.createElement("p") 	//Create an HTML element (needs to be appended)
+document.append() //Append an element
+document.remove() //Remove an element
+document.removeChild(element) 	//Remove an HTML child element
 document.appendChild(element) 	//Add an HTML element to another
 document.replaceChild(new, old) 	//Replace an HTML element
 document.write(text) 	//Write into the HTML output stream
+
+element.classList.add('new-class', 'another-class') //Adds classes to an element, can add multiple with commas separating
+element.classList.remove('class') //Removes classes from an element
+element.classList.contains('new-class') //Boolean check to see if an element contains a certain class
+element.classList.toggle('new-class') //Adds a class to an element if it does not have it, and removes the class if it does have it
+element.classList.replace('one', 'two') //Replaces an existing class with another class
+
+element.dataset //References the dataset of an element (attributes)
+element.dataset.attribute //References a specific attribute of the dataset
 
 document.querySelector('[name=item]') //Object collection, finds elements with an attribute of name with a value of item
