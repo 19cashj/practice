af41b18ae2945f9a1aecbd57142f59e9799004a2
19cashj <67474732+19cashj@users.noreply.github.com>
2021-09-25 09:05:29 -0400

    Fix

--- react-router/components/App.jsx
+++ react-router/components/App.jsx
@@ -2,20 +2,20 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from "./Home"
 import About from "./About"
+import Error from "./Error"
 
-function App() {
+export default function App() {
     return (
         <Router>
             <Switch>
-                <Route exact path="/">
+                <Route exact path="/" component={Home}>
                     <Home />
                 </Route>
-                <Route path="/about" >
+                <Route path="/about" component={About}>
                     <About />
                 </Route>
+                <Route component={Error} />
             </Switch>
         </Router>
     );
 }
-
-export default App;
