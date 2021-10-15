ea65aeecc37784c3d83f585db5c516d55338c1ef
19cashj <67474732+19cashj@users.noreply.github.com>
2021-10-15 12:22:41 -0400

    Regex day 2

--- regex/regex.js
+++ regex/regex.js
@@ -54,6 +54,13 @@ let myRegex = /[^aeiou0-9]/gi;
 let result = quoteSample.match(myRegex);
 console.log(result) // returns [ ' ', 'b', 'l', 'n', 'd', ' ', 'm', 'c', '.' ]
 
+//^ outside of a character set, searches for patterns at the beginning of strings
+let firstString = "Ricky is first and can be found.";
+let firstRegex = /^Ricky/;
+firstRegex.test(firstString); //returns true
+let notFirst = "You can't find Ricky now.";
+firstRegex.test(notFirst); //returns false
+
 // Repeat operator +, match characters that appear one or more times in a row
 let abc = "abc";
 let abca = "abca";
@@ -62,3 +69,85 @@ let abcregex = /a+/g
 abc.match(abcregex) //returns ["a"]
 abca.match(abcregex) //returns ["a", "a"]
 aabc.match(abcregex) //returns ["aa"] (this is where its useful)
+
+// Zero or more times *, like the repeat + but also includes zero or more
+let soccerWord = "gooooooooal!";
+let gPhrase = "gut feeling";
+let oPhrase = "over the moon";
+let goRegex = /go*/;
+soccerWord.match(goRegex); //returns ["goooooooo"]
+gPhrase.match(goRegex); //returns ["g"]
+oPhrase.match(goRegex); //returns null
+
+// Lazy matching, ? at the end
+// Important concept of regex is greedy and lazy mode. If in greedy mode, it matches as much of the string as possible. If lazy it matches as little as possible 
+let text = "<h1>Winter is coming</h1>";
+let myRegex = /<.*?>/;
+let result = text.match(myRegex); //returns <h1> tags
+
+// $ match ending string pattern
+let theEnding = "This is a never ending story";
+let storyRegex = /story$/;
+storyRegex.test(theEnding); //returns true
+let noEnding = "Sometimes a story will have to end";
+storyRegex.test(noEnding); //returns false
+
+// Match all letters and numbers + underscore shortcut with \w (equal to [A-Za-z0-9_])
+let longHand = /[A-Za-z0-9_]+/;
+let shortHand = /\w+/;
+let numbers = "42";
+let varNames = "important_var";
+longHand.test(numbers);
+shortHand.test(numbers);
+longHand.test(varNames);
+shortHand.test(varNames); //all return true
+// Note that there is a shortcut for the inverse of \w, \W
+// There is also a shortcut for only numbers with \d (equal to [0-9]) (also has an inverse \D)
+
+// Match whitespace with \s
+let whiteSpace = "Whitespace. Whitespace everywhere!"
+let spaceRegex = /\s/g;
+whiteSpace.match(spaceRegex); // returns [" ", " "].
+//Inverse of whitespace would be \S
+
+//Quantity specifiers with {}
+let A4 = "aaaah";
+let A2 = "aah";
+let multipleA = /a{3,5}h/;
+multipleA.test(A4); //returns true
+multipleA.test(A2); //returns false
+
+let A4 = "haaaah";
+let A2 = "haah";
+let A100 = "h" + "a".repeat(100) + "h";
+let multipleA = /ha{3,}h/; //can also specify with only a lower limit
+multipleA.test(A4); //true
+multipleA.test(A2); //false
+multipleA.test(A100); //true
+
+let A4 = "haaaah";
+let A3 = "haaah";
+let A100 = "h" + "a".repeat(100) + "h";
+let multipleHA = /ha{3}h/; //can also specify an exact number
+multipleHA.test(A4); //false
+multipleHA.test(A3); //true
+multipleHA.test(A100); //false
+
+// All or none with ?
+let american = "color";
+let british = "colour";
+let rainbowRegex= /colou?r/;
+rainbowRegex.test(american); //true
+rainbowRegex.test(british); //true
+
+// Positive and negative lookaheads
+let quit = "qu";
+let noquit = "qt";
+let quRegex= /q(?=u)/;
+let qRegex = /q(?!u)/;
+quit.match(quRegex); //returns ["q"]
+noquit.match(qRegex); //returns ["q"]
+
+let password = "abc123";
+let checkPass = /(?=\w{3,6})(?=\D*\d)/;
+checkPass.test(password); //returns true
