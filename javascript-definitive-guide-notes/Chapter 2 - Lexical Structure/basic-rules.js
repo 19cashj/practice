09ed7be80cf1aac09cab1102c6c400f7d3017dff
19cashj <67474732+19cashj@users.noreply.github.com>
2022-04-28 11:29:17 -0400

    Will be taking notes for the important bits of "Javascript The Definitive Guide" by David Flanagan

---
+++ javascript-definitive-guide-notes/Chapter 2 - Lexical Structure/basic-rules.js
@@ -0,0 +1,42 @@
+// This is a comment
+
+/* 
+
+This is a multi-line comment
+
+*/
+
+// Javascript is case-sensitive. Capitalization matters for everything.
+
+// It ignores spaces that appear between tokens in programs.
+
+// It ignores line breaks (for the most part, there is an exception)
+
+// Literals below:
+
+12 // the number twelve
+1.2 // The number one point two
+"hello word" // a string
+'Hi' // another string
+`Sup` // another another string (but you can use template literals `${}`)
+true // a boolean
+false // the other boolean
+null // Absence of an object
+
+// You can identify constants, variables, properties, functions, and classes with few limits
+// They must begin with a letter, underscore, or dollar sign. Numbers are not allowed as the first character
+// You also cannot use reserved words
+// They are:  as const export get null target void async continue extends if of this while await debugger false import return throw with break default finally
+// in set true yield case delete for instanceof static try catch do from let super typeof class else function new switch var
+// Also restricted words: enum implements interface package private protected public arguments eval
+
+// Javascript uses semicolons as seperators like other languages, but in some cases they are completely optional.
+
+a = 3
+b = 4
+// no semis needed
+
+a = 3; b = 4
+// semis needed
+
+// Do not insert line breaks between return, break, or continue
